c6424c6cd7f1dd59088d88189c7320b9055d8941
Olga Nogovitsyna <lilnyan@MacBook-Pro-Olga.local>
2021-11-03 23:19:31 +0300

    some updates

--- process.sh
+++ process.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-cd EEG_dataset
-for i in $(ls); do
-  echo "$i"
+for i in $(ls EEG_Dataset); do
+  echo "${i:0:7} №${i:8:1}"  
+  printf "Status: "
+if [ ${i:10:1} -eq 0 ]; 
+   then 
+    echo "Not interested" 
+   else 
+    echo "Interested" 
+  fi
+ printf "max value:  "
+ echo $(cat EEG_Dataset/${i} | awk -F "," 'NR>1 {print $2}' | sort -nr | head -n1)
+ printf "min value:  "
+ echo $(cat EEG_Dataset/${i} | awk -F "," 'NR>1 {print $2}' | sort -nr | tail -n1)
 done
