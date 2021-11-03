b7579a4d56e5968887fa1851bd4ec8e1f5c558b6
Olga Nogovitsyna <lilnyan@MacBook-Pro-Olga.local>
2021-11-04 01:19:57 +0300

    added std

--- process.sh
+++ process.sh
@@ -1,18 +1,11 @@
-#!/bin/bash
-echo "Subject, Status, max, min, mean, std" >> stats1.csv
+#!/bin/sh
+echo "Subject| Status |        max  |       min  |      mean  |    std"
 for i in $(ls EEG_Dataset); do
-  echo "${i:0:7} №${i:8:1}"
-  awk 
-  printf "Status: "
-if [ ${i:10:1} -eq 0 ]; 
-   then 
-    echo "Not interested" 
-   else 
-    echo "Interested" 
-  fi
- printf "max value:  "
- cat EEG_Dataset/${i} | awk -F "," 'NR>1 {print $2}' | sort -nr | head -n1
- printf "min value:  "
- cat EEG_Dataset/${i} | awk -F "," 'NR>1 {print $2}' | sort -nr | tail -n1
-
+  printf "  ${i:8:1}     " 
+  printf "  ${i:10:1}         "
+  echo "$(cat EEG_Dataset/${i} | awk -F "," 'NR>1 {print $2}' | sort -nr | head -n1)"
+  echo "$(cat EEG_Dataset/${i} | awk -F "," 'NR>1 {print $2}' | sort -nr | tail -n1)"
+  echo "$(awk -F "," '{print $0}' EEG_Dataset/$i | awk -F, 'BEGIN{x=0} {x+=$2} END{print x/NR-1}')"
+  echo "$(awk -F "," '{print $0}' EEG_Dataset/$i | awk -F "," '{delta = $2 - avg; avg += delta / NR; mean2 += delta * ($2 - avg); } END { print sqrt(mean2 / NR); }')"
 done
+python3 processing.py
