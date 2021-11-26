9e7660a8aa9cf75b095ab1be353e9ea8feb8bdf0
Olga Nogovitsyna <lilnyan@MacBook-Pro-Olga.local>
2021-11-27 02:16:53 +0300

    updated pulls.sh

--- pulls.sh
+++ pulls.sh
@@ -1,34 +1,26 @@
-sample1=$(curl -s "https://api.github.com/repos/datamove/linux-git2/pulls?state=all&page=1&per_page=100")
-count1=$(echo $sample1 | jq '.[].user.login' 2>/dev/null | grep -cs $1)
-sample2=$(curl -s "https://api.github.com/repos/datamove/linux-git2/pulls?state=all&page=2&per_page=100")
-count2=$(echo $sample2 | jq '.[].user.login' 2>/dev/null | grep -cs $1)
-sample3=$(curl -s "https://api.github.com/repos/datamove/linux-git2/pulls?state=all&page=3&per_page=100")
-count3=$(echo $sample3 | jq '.[].user.login' 2>/dev/null | grep -cs $1)
-count=$(($count1+$count2+$count3))
+LOGIN="\"$1\""
+count=0
+for i in 1 2 3 4
+do
+c=$(curl -s "https://api.github.com/repos/datamove/linux-git2/pulls?state=all&per_page=100&page=$i"| jq '.[].user.login' 2>/dev/null | grep -cs $1)
+count=$(($count+$c))
+done
 echo "PULLS $count"
 
-LOGIN="\"$1\""
-id1=$(echo $sample1 | jq -c '.[] | select(.user.login=='$LOGIN')' 2>/dev/null | jq '.number' | tail -1)
-id2=$(echo $sample2 | jq -c '.[] | select(.user.login=='$LOGIN')' 2>/dev/null | jq '.number' | tail -1)
-id3=$(echo $sample3 | jq -c '.[] | select(.user.login=='$LOGIN')' 2>/dev/null | jq '.number' | tail -1)
-
-if [ -z "$id3" ]
+id=""
+for i in 4 3 2 1
+do
+if [ -z "$id" ]
 then
-  if [ -z "$id2" ]
-  then
-    id=$(echo "id1")
-  else
-    id=$(echo "id2")
-  fi
- else
-  id=$(echo "id3")
+id=$(curl -s "https://api.github.com/repos/datamove/linux-git2/pulls?state=all&per_page=100&page=$i" | jq -c '.[] | select(.user.login=='$LOGIN')' 2>/dev/null | jq '.number' | tail -1)
 fi
+done
 echo "EARLIEST $id"
 
 merge_status=$(curl -s "https://api.github.com/repos/datamove/linux-git2/pulls/$id" | jq '.merged_at')
-if [[ "$merge_status" == "null" ]]
+if [ "$merge_status" == "null" ]
 then
-echo "MERGED 1"
-else
 echo "MERGED 0"
+else
+echo "MERGED 1"
 fi
