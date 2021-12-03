2d772bf91a5bdfb63c7ce273870b93d3770c8752
Olga Nogovitsyna <lilnyan@MacBook-Pro-Olga.local>
2021-12-03 05:52:08 +0300

    edit script

--- project2/.github/script.sh
+++ project2/.github/script.sh
@@ -1,1 +1,11 @@
-echo "Hello World!"
+formats=`find . -type f -not -path '*/\.git/*' | perl -ne 'print $1 if m/\.([^.\/]+)$/' | sort -u`
+for i in $formats
+do
+files=`find . -type f -name "*.$i"`
+count=0
+for j in $files
+do
+count=$count+`wc -m $j`
+done
+echo $i $count
+done
