1de983027a0848ec673d3477449fede2945390be
Olga Nogovitsyna <lilnyan@MacBook-Pro-Olga.local>
2021-12-04 23:46:04 +0300

    add count

--- project2/.github/script.sh
+++ project2/.github/script.sh
@@ -1,11 +1,16 @@
 formats=`find . -type f -not -path '*/\.git/*' | perl -ne 'print $1 if m/\.([^.\/]+)$/' | sort -u`
+values=''
+total=0
 for i in $formats
 do
 files=`find . -type f -name "*.$i"`
 count=0
 for j in $files
 do
-count=$count+`wc -m $j`
+count="$(($count+`cat $j | wc -m`))"
 done
+total="$(($total+$count))"
 echo $i $count
 done
+echo $total
+
