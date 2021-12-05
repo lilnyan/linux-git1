86ce9836c58b03d0a0f6eb5853dfd85033dae595
Olga Nogovitsyna <lilnyan@MacBook-Pro-Olga.local>
2021-12-05 05:10:03 +0300

    add percentage

--- project2/.github/script.sh
+++ project2/.github/script.sh
@@ -1,6 +1,5 @@
 formats=`find . -type f -not -path '*/\.git/*' | perl -ne 'print $1 if m/\.([^.\/]+)$/' | sort -u`
-values=''
-total=0
+total_count=$(wc -m `find . -type f -not -path '*/\.git/*'` | tail -1 | sed 's/ total.*//' | bc)
 for i in $formats
 do
 files=`find . -type f -name "*.$i"`
@@ -9,8 +8,9 @@ for j in $files
 do
 count="$(($count+`cat $j | wc -m`))"
 done
-total="$(($total+$count))"
+count=`jq -n $count*100/$total_count`
 echo $i $count
 done
-echo $total
-
+last_count=$(wc -m `find . -type f ! -name "*.*" -not -path '*/\.git/*'` | tail -1 | sed 's/ total.*//' | bc)
+last_count=`jq -n $last_count*100/$total_count`
+echo "without extension" $last_count
